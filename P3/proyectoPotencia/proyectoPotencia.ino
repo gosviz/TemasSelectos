8b3e6a6cbf787db90dbc62cf6c9d8c0c48d0d615
Luis Antonio <antonio.dgr.ink@gmail.com>
2018-09-26 16:49:58 -0500

    Agregada etapa de potencia

--- P3/proyectoPotencia/proyectoPotencia.ino
+++ P3/proyectoPotencia/proyectoPotencia.ino
@@ -1,45 +1,56 @@
-#include<string.h> //para strcmp() compara cadenas
-#include<math.h>
 
 //Variables globales
-int i;
-short int contacto = 0,
-          foto_Resistencia_Digital;
-double  foto_Resistencia_Analogica = 0,
-        infrarojo = 0;
-char buff[20];  //Buffer serial;
+char buff[20];  //Buffer serial
+int vel=0,
+    vel2= 0;
 
 void setup(){
   //Inicialización y declaración de variables
-  pinMode(2,INPUT);
-  pinMode(3,INPUT);
-  pinMode(A0,INPUT);
-  pinMode(A1,INPUT);
-  pinMode(A2,INPUT);
-  
+  pinMode(3,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(13,OUTPUT);
   Serial.begin(9600);   //Habilita comunicación serial
   Serial.setTimeout(100); //100ms máximo para caracter nulo
+ // while(!Serial){}
+  Serial.println("Instrucciones para el motor:");
+  Serial.println("1) M1 speed (PWM)\n 2) M2 speed (PWM)\n 3) M1 off\n 4) M2 off");
+  Serial.println("Al escribir positivo o negativo el valor del PWM se cambia el sentido de giro");
+  Serial.println("Escribe la instuccion completa");
 }
 
 void loop(){
-  
-}
-/**/
-int velocidad = 0;
-void serialEvent(){
-  if(Serial.available()){ //Sí, hay datos en el buffer
-    //Carga los datos en un buffer auxiliar
-    Serial.readBytesUntil('\n', buff, 20);
-    //Serial.println(buff);
-    if(!strncmp(buff, "M1 speed", 8)){//Compara el buffer
-      velocidad = atoi(buff+8);
-      if(velocidad <128 && velocidad >-128){
-        velocidad = velocidad +128;
-        analogWrite(3,velocidad);
-      }
-    }else{
-        Serial.println("Ningun sensor con esa descripción");
+  if(Serial.available()){
+    Serial.readBytes(buff,20);
+    Serial.println(buff);
+    if(!strncmp(buff,"M1 speed",8)){
+      vel=atoi(buff+8);
+      Serial.println(vel);
+       if(vel<=127 && vel>=-127){
+        vel=vel+127;
+        analogWrite(5,vel);
+       }
     }
-    memset(buff,0,sizeof(buff));//Limpia el buffer
+    if(!strncmp(buff,"M2 speed",8)){
+       vel2=atoi(buff+8);
+       Serial.println(vel2);
+       if(vel2<=127 && vel2>=-127){
+        vel2=vel2+127;
+        analogWrite(3,vel2);
+        }
+       }
+    if(!strcmp(buff, "M1 off")){
+      vel=0;
+      analogWrite(10,vel);
+      }
+    if(!strcmp(buff, "M2 off")){
+      vel=0;
+      analogWrite(9,vel);
+      }
+    else{
+      Serial.println("No existe ese comando");
+    }
+    memset(buff,0,sizeof(buff));
+    }
   }
-}
+
