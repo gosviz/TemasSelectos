97be87f9ec0301c09d9260811264dd4235a5eede
Luis Antonio <antonio.dgr.ink@gmail.com>
2018-09-26 22:03:31 -0500

    Potencia correjido

--- P3/proyectoPotencia/proyectoPotencia.ino
+++ P3/proyectoPotencia/proyectoPotencia.ino
@@ -1,12 +1,16 @@
 
 //Variables globales
 char buff[20];  //Buffer serial
-int vel=0,
-    vel2= 0;
+int vel,
+    vel2;
 
 void setup(){
   //Inicialización y declaración de variables
   pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(2,OUTPUT);
+
+  pinMode(6,OUTPUT);
   pinMode(11,OUTPUT);
   pinMode(5,OUTPUT);
   pinMode(13,OUTPUT);
@@ -16,11 +20,24 @@ void setup(){
   Serial.println("Instrucciones para el motor:");
   Serial.println("1) M1 speed (PWM)\n 2) M2 speed (PWM)\n 3) M1 off\n 4) M2 off");
   Serial.println("Al escribir positivo o negativo el valor del PWM se cambia el sentido de giro");
+  
   Serial.println("Escribe la instuccion completa");
-}
+  digitalWrite(4,LOW);
+  digitalWrite(2,LOW);
+  analogWrite(5,127);
+  analogWrite(6,127);
+  digitalWrite(4,HIGH);
+  digitalWrite(2,HIGH);
+  analogWrite(5,127+30);
+  analogWrite(6,127+30);
+} 
 
 void loop(){
-  if(Serial.available()){
+}
+void serialEvent(){
+  analogWrite(5,127+30);
+  analogWrite(6,127+30);
+  /*if(Serial.available()){
     Serial.readBytes(buff,20);
     Serial.println(buff);
     if(!strncmp(buff,"M1 speed",8)){
@@ -35,22 +52,23 @@ void loop(){
        vel2=atoi(buff+8);
        Serial.println(vel2);
        if(vel2<=127 && vel2>=-127){
+          
         vel2=vel2+127;
         analogWrite(3,vel2);
         }
        }
     if(!strcmp(buff, "M1 off")){
-      vel=0;
-      analogWrite(10,vel);
+      
+      digitalWrite(2,0);
       }
     if(!strcmp(buff, "M2 off")){
-      vel=0;
-      analogWrite(9,vel);
+      
+      analogWrite(4,0);
       }
     else{
       Serial.println("No existe ese comando");
     }
     memset(buff,0,sizeof(buff));
-    }
+    }*/
   }
 
